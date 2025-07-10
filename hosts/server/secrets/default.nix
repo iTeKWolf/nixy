a811ba3c30319adeb40a0cdbc2726893ce75d7b7
iTeKWolf <catonquentin150802@gmail.com>
2025-07-10 18:04:25 +0200

    first secrets

--- hosts/server/secrets/default.nix
+++ hosts/server/secrets/default.nix
@@ -1,32 +1,22 @@
 {pkgs, ...}: {
   sops = {
-    age.keyFile = "/home/hadi/.config/sops/age/keys.txt";
+    age.keyFile = "/home/wolf/.config/sops/age/keys.txt";
     defaultSopsFile = ./secrets.yaml;
     secrets = {
       sshconfig = {
-        owner = "hadi";
-        path = "/home/hadi/.ssh/config";
+        owner = "wolf";
+        path = "/home/wolf/.ssh/config";
         mode = "0600";
       };
       github-key = {
-        owner = "hadi";
-        path = "/home/hadi/.ssh/github";
+        owner = "wolf";
+        path = "/home/wolf/.ssh/github";
         mode = "0600";
       };
-      signing-key = {
-        owner = "hadi";
-        path = "/home/hadi/.ssh/key";
-        mode = "0600";
-      };
-      signing-pub-key = {
-        owner = "hadi";
-        path = "/home/hadi/.ssh/key.pub";
-        mode = "0600";
-      };
-      cloudflare-dns-token = {path = "/etc/cloudflare/dnskey.txt";};
-      nextcloud-pwd = {path = "/etc/nextcloud/pwd.txt";};
-      adguard-pwd = {};
-      hoarder = {};
+      #cloudflare-dns-token = {path = "/etc/cloudflare/dnskey.txt";};
+      #nextcloud-pwd = {path = "/etc/nextcloud/pwd.txt";};
+      #adguard-pwd = {};
+      #hoarder = {};
     };
   };
 
